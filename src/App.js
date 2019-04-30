7031c66a9fc7af42e65acb3a07087ae48db37825
aloknecessary <aloks@byteridge.com>
2019-04-30 12:46:48 +0530

    added a reset button & changed css class of buttons

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ class Counter extends Component {
          super();
          this.handleIncrement = this.handleIncrement.bind(this);
          this.handleDecrement = this.handleDecrement.bind(this);
+         this.reset = this.reset.bind(this);
      }
      
     render() { 
@@ -18,7 +19,8 @@ class Counter extends Component {
           <p>Counter Sample - React</p>
             <span className={this.getBadgeClasses()} >{this.state.count}</span>
             <div className="action-buttons">
-              <button className="btn btn-secondary btn-sm" onClick={this.handleDecrement}>Decrement</button>
+              <button className="btn btn-info btn-sm" onClick={this.handleDecrement}>Decrement</button>
+              <button className="btn btn-light btn-sm inc-btn" onClick={this.reset}>Reset</button>
               <button className="btn btn-success btn-sm inc-btn" onClick={this.handleIncrement}>Increment</button>
             </div>
             
@@ -26,7 +28,7 @@ class Counter extends Component {
         );
     }
     getBadgeClasses(){
-        return "badge m-2 badge-style badge-" + (this.state.count <= 0 ? 'warning' : 'primary');
+        return "badge m-2 badge-style badge-" + (this.state.count === 0 ? 'warning' : this.state.count > 0 ? 'primary' : 'danger');
     }
     
     handleIncrement(){
@@ -39,7 +41,11 @@ class Counter extends Component {
       this.setState({
           count: this.state.count - 1
       })
-  }
+    }
+
+    reset = () => {
+        this.setState({count: 0 });
+    }
 }
  
 export default Counter;
